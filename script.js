c9971b1732f86f295a6fe171165e70e3a3ccc1c4
johnathan olivas <jj.galvan13@yahoo.com>
2020-04-09 18:21:52 -0600

    created variables

---
+++ script.js
@@ -0,0 +1,4 @@
+var citiesList = $("#cities")
+var cities = []
+var cityName, forecastQueryURL, weatherData, currentWeatherIcon, currentWeatherIconEl, weathericon, currentTemp, weatherCard, cityDate, temp, humidity, windspeed, forecastQueryParams, fiveDayList;
+// grabs URL for current weather
