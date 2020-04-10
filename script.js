708a2e58d00bd055abaa3f19731d97e289f4635d
johnathan olivas <jj.galvan13@yahoo.com>
2020-04-09 18:36:59 -0600

    created buildQueryUrl

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var citiesList = $("#cities")
 var cities = []
 var cityName, forecastQueryURL, weatherData, currentWeatherIcon, currentWeatherIconEl, weathericon, currentTemp, weatherCard, cityDate, temp, humidity, windspeed, forecastQueryParams, fiveDayList;
-// grabs URL for current weather
+
+function buildQueryUrl() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
+    var queryParams = { "appid": "7a0417bb28fd390f6e702d0fcf1d9a1a" };
+    queryParams.q = $("#search-term").val().trim();
+    queryParams.units = "imperial"
+    return queryURL + $.param(queryParams);
+}
